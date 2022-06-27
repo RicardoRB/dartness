1d773ec1175eb7335c6d51baaf991af72a76095b
RicardoRB <ricardoromebeni@gmail.com>
2022-06-27 14:34:12 +0200

    Created router_handler.dart in order to refactor a bit the code

--- bin/dartness.dart
+++ bin/dartness.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 import 'dart:mirrors';
 
@@ -6,8 +5,7 @@ import 'package:collection/collection.dart';
 import 'package:dartness/bind/annotation/bind.dart';
 import 'package:dartness/bind/annotation/controller.dart';
 import 'package:dartness/bind/annotation/get.dart';
-import 'package:dartness/bind/annotation/query_param.dart';
-import 'package:logger/logger.dart';
+import 'package:dartness/route/router_handler.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -15,7 +13,6 @@ import 'package:shelf_router/shelf_router.dart';
 /// A server that delivers content, such as web pages, using the HTTP protocol
 /// by [HttpServer].
 class Dartness {
-  final logger = Logger();
   HttpServer? _server;
   final Set<Object> _controllers = {};
 
@@ -96,116 +93,15 @@ class Dartness {
         if (metadata.type.isSubtypeOf(reflectClass(Bind))) {
           final bind = metadata.reflectee as Bind;
           final path = '${ctlReflectee.path}${bind.path}';
+          final handler = RouterHandler(clazzDeclaration, method);
           _router.add(
             bind.toString(),
             path,
-            _handleRoute(clazzDeclaration, method),
+            handler.handleRoute(),
           );
         }
       }
     }
     _controllers.add(controller);
-  }
-
-  /// Handles the route's response and invoke the [method] in [clazzDeclaration]
-  Function _handleRoute(
-      final ClassMirror clazzDeclaration, final MethodMirror method) {
-    return (final Request request, [final Object? extras]) async {
-      try {
-        //TODO query params
-        final Map<String, String> pathParams = Map.of(request.params)
-          ..removeWhere(
-              (key, value) => request.url.queryParameters.containsKey(key));
-        final methodParams = [];
-        if (pathParams.isNotEmpty) {
-          final params = _getPathParamsValues(method, pathParams);
-          methodParams.addAll(params);
-        }
-        if (request.url.queryParameters.isNotEmpty) {
-          final queryParams =
-              _getQueryParamsValues(method, request.url.queryParameters);
-          methodParams.addAll(queryParams);
-        }
-        final response =
-            clazzDeclaration.invoke(method.simpleName, methodParams);
-        var result = response.reflectee;
-        if (result is Future) {
-          return Response.ok(await result);
-        } else if (result is Response) {
-          return result;
-        } else if (result is Iterable || result is Map || result is Object) {
-          return Response.ok(jsonEncode(result));
-        } else {
-          return Response.ok(result);
-        }
-      } catch (e, stack) {
-        logger.e('Error handling route', e, stack);
-        return Response.internalServerError(body: e.toString());
-      }
-    };
-  }
-
-  /// Returns the values of the [method] parameters based on the [pathParams]
-  ///
-  /// In order to invoke the [method] the parameters must be in the same order
-  /// and must has the same name.
-  List<Object> _getPathParamsValues(
-      final MethodMirror method, final Map<String, String> pathParams) {
-    final List<Object> params = [];
-    for (final methodParam in method.parameters) {
-      final value = _getParamValue(methodParam, pathParams);
-      _addTypedParam(methodParam, params, value);
-    }
-    return params;
-  }
-
-  /// Return the param value by the [ParameterMirror] and the map [params]
-  String _getParamValue(
-      final ParameterMirror methodParam, final Map<String, String> params) {
-    final paramName = MirrorSystem.getName(methodParam.simpleName);
-    final value = params[paramName];
-    if (value == null) {
-      throw ArgumentError.value(methodParam, 'methodParam',
-          'missing parameter ${methodParam.simpleName}');
-    }
-    return value;
-  }
-
-  /// Gets the [method] parameters values from the [queryParams].
-  /// The [queryParams] must be in the same order as the [method] parameters.
-  List<Object> _getQueryParamsValues(
-      final MethodMirror method, final Map<String, String> queryParameters) {
-    final List<Object> params = [];
-    // TODO: check if the query params are in the same order as the method params
-    final methodQueryParams = method.parameters.where((element) {
-      return element.metadata.any((metadata) {
-        return metadata.reflectee.runtimeType == QueryParam;
-      });
-    });
-
-    for (final methodParam in methodQueryParams) {
-      final value = _getParamValue(methodParam, queryParameters);
-      _addTypedParam(methodParam, params, value);
-    }
-    return params;
-  }
-
-  /// Adds the [value] to the [params] based on the [methodParam] reflected type.
-  void _addTypedParam(
-      ParameterMirror methodParam, List<Object> params, String value) {
-    if (methodParam.type.reflectedType == int) {
-      params.add(int.parse(value));
-    } else if (methodParam.type.reflectedType == double) {
-      params.add(double.parse(value));
-    } else if (methodParam.type.reflectedType == String) {
-      params.add(value);
-    } else if (methodParam.type.reflectedType == bool) {
-      params.add(value == 'true');
-    } else if (methodParam.type.reflectedType == Object) {
-      params.add(value);
-    } else {
-      throw ArgumentError.value(
-          methodParam.type.reflectedType, 'methodParam.type.reflectedType');
-    }
   }
 }
---
+++ lib/route/router_handler.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+import 'dart:mirrors';
+
+import 'package:dartness/bind/annotation/query_param.dart';
+import 'package:logger/logger.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class RouterHandler {
+  final _logger = Logger();
+
+  final ClassMirror _clazzMirror;
+  final MethodMirror _methodMirror;
+
+  RouterHandler(this._clazzMirror, this._methodMirror);
+
+  /// Handles the route's response and invoke the [_methodMirror] in [_clazzMirror]
+  Function handleRoute() {
+    return (final Request request, [final Object? extras]) async {
+      try {
+        final Map<String, String> pathParams = Map.of(request.params)
+          ..removeWhere((key, value) {
+            return request.url.queryParameters.containsKey(key);
+          });
+        final methodParams = [];
+        if (pathParams.isNotEmpty) {
+          final params = _getPathParamsValues(_methodMirror, pathParams);
+          methodParams.addAll(params);
+        }
+        if (request.url.queryParameters.isNotEmpty) {
+          final queryParams =
+              _getQueryParamsValues(_methodMirror, request.url.queryParameters);
+          methodParams.addAll(queryParams);
+        }
+        final response =
+            _clazzMirror.invoke(_methodMirror.simpleName, methodParams);
+        var result = response.reflectee;
+        if (result is Future) {
+          return Response.ok(await result);
+        } else if (result is Response) {
+          return result;
+        } else if (result is Iterable || result is Map || result is Object) {
+          return Response.ok(jsonEncode(result));
+        } else {
+          return Response.ok(result);
+        }
+      } catch (e, stack) {
+        _logger.e('Error handling route', e, stack);
+        return Response.internalServerError(body: e.toString());
+      }
+    };
+  }
+
+  /// Returns the values of the [method] parameters based on the [pathParams]
+  ///
+  /// In order to invoke the [method] the parameters must be in the same order
+  /// and must has the same name.
+  List<Object> _getPathParamsValues(
+      final MethodMirror method, final Map<String, String> pathParams) {
+    final List<Object> params = [];
+    for (final methodParam in method.parameters) {
+      final value = _getParamValue(methodParam, pathParams);
+      _addTypedParam(methodParam, params, value);
+    }
+    return params;
+  }
+
+  /// Gets the [method] parameters values from the [queryParams].
+  /// The [queryParams] must be in the same order as the [method] parameters.
+  List<Object> _getQueryParamsValues(
+      final MethodMirror method, final Map<String, String> queryParameters) {
+    final List<Object> params = [];
+    // TODO: check if the query params are in the same order as the method params
+    final methodQueryParams = method.parameters.where((element) {
+      return element.metadata.any((metadata) {
+        return metadata.reflectee.runtimeType == QueryParam;
+      });
+    });
+
+    for (final methodParam in methodQueryParams) {
+      final value = _getParamValue(methodParam, queryParameters);
+      _addTypedParam(methodParam, params, value);
+    }
+    return params;
+  }
+
+  /// Return the param value by the [ParameterMirror] and the map [params]
+  String _getParamValue(
+      final ParameterMirror methodParam, final Map<String, String> params) {
+    final paramName = MirrorSystem.getName(methodParam.simpleName);
+    final value = params[paramName];
+    if (value == null) {
+      throw ArgumentError.value(methodParam, 'methodParam',
+          'missing parameter ${methodParam.simpleName}');
+    }
+    return value;
+  }
+
+  /// Adds the [value] to the [params] based on the [methodParam] reflected type.
+  void _addTypedParam(
+      ParameterMirror methodParam, List<Object> params, String value) {
+    if (methodParam.type.reflectedType == int) {
+      params.add(int.parse(value));
+    } else if (methodParam.type.reflectedType == double) {
+      params.add(double.parse(value));
+    } else if (methodParam.type.reflectedType == String) {
+      params.add(value);
+    } else if (methodParam.type.reflectedType == bool) {
+      params.add(value == 'true');
+    } else if (methodParam.type.reflectedType == Object) {
+      params.add(value);
+    } else {
+      throw ArgumentError.value(
+          methodParam.type.reflectedType, 'methodParam.type.reflectedType');
+    }
+  }
+}
