7a7ac9c1065b40733ba41a2d30e52d0733322b04
RicardoRB <ricardoromebeni@gmail.com>
2022-07-03 15:08:27 +0200

    Created first_steps.md

--- example/main.dart
+++ example/main.dart
@@ -5,6 +5,7 @@ import 'example_controller.dart';
 void main() async {
   final app = Dartness(
     port: 3000,
+    controllers: [ExampleController()],
   );
   app.addController(ExampleController());
   await app.create();
