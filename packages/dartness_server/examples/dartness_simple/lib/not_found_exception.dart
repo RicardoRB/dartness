029181e5d62473ed79a2bbae5e9359792b6fe45e
RicardoRB <ricardoromebeni@gmail.com>
2022-08-19 13:33:48 +0200

    Moved examples to root and simple uses dartness_server with dartness_generator

--- packages/dartness_server/examples/dartness_simple/lib/not_found_exception.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-import 'package:dartness_server/exception.dart';
-
-class NotFoundException extends HttpStatusException {
-  const NotFoundException(String message) : super(message, HttpStatus.notFound);
-}
