15daa2eccab73b813be93dd4338537d403a1ac1f
RicardoRB <ricardoromebeni@gmail.com>
2023-10-31 17:38:18 +0100

    Improved simple example structure

--- examples/dartness_simple/lib/src/app.dart
+++ examples/dartness_simple/lib/src/app.dart
@@ -1,14 +1,12 @@
 import 'package:dartness_server/dartness.dart';
 import 'package:dartness_server/modules.dart';
 import 'package:dartness_server/server.dart';
-import 'package:example/src/controllers/city/city_module.dart';
-import 'package:example/src/controllers/user/user_module.dart';
 import 'package:example/src/services/city_service.dart';
 
-import 'controllers/city/city_controller.dart';
 import 'controllers/health_controller.dart';
-import 'controllers/user/user_controller.dart';
 import 'error_handlers/example_error_handler.dart';
+import 'modules/city_module.dart';
+import 'modules/user_module.dart';
 
 part 'app.g.dart';
 
--- examples/dartness_simple/lib/src/modules/user_module.dart
+++ examples/dartness_simple/lib/src/modules/user_module.dart
@@ -1,6 +1,6 @@
 import 'package:dartness_server/modules.dart';
 
-import '../user/user_controller.dart';
+import '../controllers/user_controller.dart';
 
 const userModule = Module(
   metadata: ModuleMetadata(
