0211dbeb9bc6fa88e725ece18251a4493b4ae477
RicardoRB <ricardoromebeni@gmail.com>
2023-11-03 17:22:49 +0100

    inject.dart should not be abstract to be an annotation

--- packages/dartness_server/lib/src/app/annotation/inject.dart
+++ packages/dartness_server/lib/src/app/annotation/inject.dart
@@ -1,6 +1,6 @@
 /// Annotation that marks a constructor parameter as a target for
 /// dependence injection
-abstract class Inject {
+class Inject {
   /// Lookup key for the provider to be injected
   final String? name;
 
