35f3e8c318ae7e09db8dd17f70d3f793800bfddf
RicardoRB <ricardoromebeni@gmail.com>
2023-11-03 15:38:39 +0100

    Created inject.dart

---
+++ packages/dartness_server/lib/src/app/annotation/inject.dart
@@ -0,0 +1,8 @@
+/// Annotation that marks a constructor parameter as a target for
+/// dependence injection
+abstract class Inject {
+  /// Lookup key for the provider to be injected
+  final String? name;
+
+  const Inject(this.name);
+}
