7f850bbd204d331ba9fded858ec1b9c793be8cce
RicardoRB <ricardoromebeni@gmail.com>
2023-12-11 14:59:58 +0100

    Fixed exception extends

--- examples/dartness_simple/lib/src/error_handlers/not_found_exception.dart
+++ examples/dartness_simple/lib/src/error_handlers/not_found_exception.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 
 import 'package:dartness_server/exception.dart';
 
-class NotFoundException extends HttpStatusException {
+class NotFoundException extends HttpStatusCodeException {
   const NotFoundException(String message) : super(message, HttpStatus.notFound);
 }
