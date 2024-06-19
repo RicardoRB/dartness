56f8edec9b315f743cf0a77af5f2253c51367b0c
Ricardo Romero <ricardoromebeni@gmail.com>
2024-06-19 21:59:09 +0200

    Fixed format

--- packages/dartness_server/lib/src/configuration/injectable.dart
+++ packages/dartness_server/lib/src/configuration/injectable.dart
@@ -1,7 +1,7 @@
 /// A metadata annotation used to mark a class as available to the dependency injection system.
 ///
 /// This annotation can be applied to classes which should be instantiated or managed by the DI framework.
-/// Marking a class with `@Injectable` indicates to the DI framework that an instance of the class 
+/// Marking a class with `@Injectable` indicates to the DI framework that an instance of the class
 /// can be created and provided to other parts of the application that require it.
 ///
 /// Usage:
@@ -14,7 +14,7 @@
 ///
 /// The DI framework will then manage the lifecycle of the instantiated `MyService` objects,
 /// allowing for dependency management and injection into other classes that depend on `MyService`.
-/// 
+///
 class Injectable {
   /// Creates a new instance of `Injectable`.
   ///
